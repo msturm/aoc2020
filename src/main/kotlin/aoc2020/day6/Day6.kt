4c85084d54692b6c72c821170f039fc0fb72a941
Martin Sturm <msturm@ebay.com>
2020-12-06 16:55:33 +0100

    Add oneliners

--- src/main/kotlin/aoc2020/day6/Day6.kt
+++ src/main/kotlin/aoc2020/day6/Day6.kt
@@ -4,6 +4,9 @@ fun main() {
     val input = {}.javaClass.getResource("/day6-2020-1.input").readText()
     println("Day 6 part 1: ${QuestionAnalyzer(input).countAllQuestionairesAnyoneYes()}")
     println("Day 6 part 2: ${QuestionAnalyzer(input).countAllQuestionairesEveryoneYes()}")
+
+    println("Day 6 part 1 as oneliner: " + input.split("\n\n").fold(0) {acc, el -> acc + el.toSet().filterNot { it == '\n' }.count()})
+    println("Day 6 part 2 as oneliner: " + input.split("\n\n").fold(0) {acc, el -> acc + el.trim().split("\n").fold(('a'..'z').asSequence().toSet()) { acc2, answers -> answers.toSet().intersect(acc2)}.count()})
 }
 
 class QuestionAnalyzer(val input: String = "") {
